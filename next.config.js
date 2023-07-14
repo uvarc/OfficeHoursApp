d87c8edd9d136421c9076573504db4009d0e293d
uvamobiledev <nje2n@virginia.edu>
2023-07-13 21:59:16 -0400

    Added API Logs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverRuntimeConfig: {
+    // Enable server logs for Next.js API routes
+    INTERNAL_API_LOGS: true,
+  },
+  
   reactStrictMode: true,
 }
+
 
 module.exports = nextConfig
