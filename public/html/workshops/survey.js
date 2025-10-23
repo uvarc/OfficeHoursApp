7555f4b75d479b07b20cb490b16acbe798448040
Diana Lin <lindiana1206@gmail.com>
2025-10-23 17:37:47 -0400

    move to prod backend

--- public/html/workshops/survey.js
+++ public/html/workshops/survey.js
@@ -1,6 +1,6 @@
 const allResponsesDiv = document.getElementById('allResponses');
 
-const backendUrl = window.location.origin === 'http://localhost:3000' ? 'http://localhost:5000' : 'https://uvarc-unified-service-test.pods.uvarc.io';
+const backendUrl = window.location.origin === 'http://localhost:3000' ? 'http://localhost:5000' : 'https://uvarc-unified-service-prod.pods.uvarc.io';
 // const backendUrl = 'https://uvarc-unified-service-test.pods.uvarc.io';
 
 const charts = {};
